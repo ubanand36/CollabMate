08e6ec75dfab4a5b1e10f38412fa392a1255e41a
Anand Kumar Patel <ak0591640@gmail.com>
2026-03-13 16:31:39 +0530

    Change server run configuration for Flask app
    Updated the server run configuration to allow binding to all interfaces and set the port from environment variables.

--- collabmate/app.py
+++ collabmate/app.py
@@ -64,4 +64,5 @@ def search():
 # ---------------- RUN SERVER ---------------- #
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
